2b038638a35aa47a64e48052e4e0248c8d27529d
Allan Chávez <allancss2009@hotmail.com>
2023-11-06 20:12:48 -0600

    feat: print error message on the toast of the axios interceptor  if there's any

--- src/services/api/responseHandlers/errorHandler.js
+++ src/services/api/responseHandlers/errorHandler.js
@@ -1,26 +1,32 @@
-import { toast } from 'vue3-toastify';
-import 'vue3-toastify/dist/index.css';
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 
-
-async function evaluateResponse(error){
-    console.log('evaluateResponse');
-    console.log(error.response);
-    if(error.response.status === 200){
-        toast.success('Success');
-    }else if(error.response.status === 400 || error.response.status === 500){
-        toast.error('Oops! Something went wrong, please try again.',
-        {
-          autoClose: 1500,
-          hideProgressBar:  false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          position: 'bottom-right'
-          
-        });
-    } 
-
+async function evaluateResponse(error) {
+  console.log("evaluateResponse");
+  console.log(error.response);
+  if (error.response.status === 200) {
+    // toast.success('Success');
+  } else if (error.response.status === 400 || error.response.status === 500) {
+    if (error.response.data.error) {
+      toast.error(error.response.data.error, {
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        position: "bottom-center",
+      });
+    } else {
+      toast.error("Oops! Something went wrong, please try again.", {
+        autoClose: 1500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        position: "bottom-right",
+      });
+    }
+  }
 }
 
 export default {
-    evaluateResponse
-}
+  evaluateResponse,
+};
