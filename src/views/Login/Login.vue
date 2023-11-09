6b1fdc920a80ee8acbcbd512dbee22093d25c1db
Allan Chávez <allancss2009@hotmail.com>
2023-11-08 18:24:39 -0600

    refactor: Login component code with prettier

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -6,9 +6,8 @@ import { useRouter } from "vue-router";
 import CryptoJS from "crypto-js";
 import usersService from "../../services/usersService.js";
 import Cookies from "js-cookie";
-import { toast } from 'vue3-toastify';
-import 'vue3-toastify/dist/index.css';
-
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 
 const { login } = usersService;
 const router = useRouter();
@@ -28,23 +27,25 @@ const authentication = async () => {
   loading.value = true;
   const isValid = await validateForm();
   if (!isValid) return;
-  try{
+  try {
     // I have a env variable using vite. it's called PASSPHRASE. The syntax to get it from the code is import.meta.env.PASSPHRASE
-    const hashedPassword =  CryptoJS.AES.encrypt(user.value.password, PASSPHRASE.value).toString()
+    const hashedPassword = CryptoJS.AES.encrypt(
+      user.value.password,
+      PASSPHRASE.value
+    ).toString();
     const userCredentials = {
       email: user.value.email,
       password: hashedPassword,
     };
     const response = await login(userCredentials);
     await handleLoginResponse(response);
-  }catch(err){
-      console.log(err);
-      toast.error("An error just occurred!", {
-        position: "top-right",
-        autoClose: 2000,
-      });
-  }finally
-  {
+  } catch (err) {
+    console.log(err);
+    toast.error("An error just occurred!", {
+      position: "top-right",
+      autoClose: 2000,
+    });
+  } finally {
     loading.value = false;
   }
 };
@@ -53,35 +54,33 @@ const validateForm = async () => {
   const isValid = await loginForm.value.validate();
   console.log("The form is valid: ", isValid.valid);
   return isValid.valid;
-};  
+};
 
-const handleLoginResponse = async (response) =>{
-  if(response.status === 200){
-      console.log("response.data: ", response.data);
-      Cookies.set("token", response.data.token);
-      Cookies.set("user", response.data.username);
+const handleLoginResponse = async (response) => {
+  if (response.status === 200) {
+    console.log("response.data: ", response.data);
+    Cookies.set("token", response.data.token);
+    Cookies.set("user", response.data.username);
 
-      const jwt = Cookies.get("token");
-      console.log("jwt: ", jwt);
+    const jwt = Cookies.get("token");
+    console.log("jwt: ", jwt);
 
-      setTimeout(() => {
-        router.push("/home");
-      }, 2000);
-      toast.success("Log in successful!", {
-        position: "top-right",
-        autoClose: 2000,
-      });
-    }
-    else{
-         // A validation if the credentials are wrong.
-      toast.error("Something went wrong logging in!", {
-        position: "top-right",
-        duration: 2000,
-      });
-    }
-    loading.value = false;
-
-}
+    setTimeout(() => {
+      router.push("/home");
+    }, 2000);
+    toast.success("Log in successful!", {
+      position: "top-right",
+      autoClose: 2000,
+    });
+  } else {
+    // A validation if the credentials are wrong.
+    toast.error("Something went wrong logging in!", {
+      position: "top-right",
+      duration: 2000,
+    });
+  }
+  loading.value = false;
+};
 
 // Login form validation rules
 const emailRules = [
@@ -94,7 +93,7 @@ const passwordRules = [
   (v) => (v && v.length >= 8) || "Password must be larger than 8 characters",
 ];
 
-// The mdi-close of the clearable option of the v-text-field is not working. 
+// The mdi-close of the clearable option of the v-text-field is not working.
 </script>
 
 
@@ -106,9 +105,7 @@ const passwordRules = [
     >
       <div class="sm:px-44 w-full h-4/6">
         <div class="bg-white shadow-md rounded-2xl">
-          <v-form
-            ref="loginForm"
-          fast-fail @submit.prevent="authentication">
+          <v-form ref="loginForm" fast-fail @submit.prevent="authentication">
             <div class="px-16 py-8 grid gap-y-6">
               <div id="title-page" class="my-10">
                 <h1 class="text-4xl text-center font-bold text-purple-900">
@@ -125,19 +122,20 @@ const passwordRules = [
               </div>
               <div>
                 <v-text-field
-                   ref="passwordInput"
+                  ref="passwordInput"
                   class="text-purple-900"
                   label="Password"
                   :type="showPassword ? 'text' : 'password'"
                   :rules="passwordRules"
                   v-model="user.password"
                 >
-                <span class="material-icons-outlined absolute right-3 top-5 cursor-pointer text-purple-800 
-                "  @click="showPassword = !showPassword">
-                {{ showPassword ? 'visibility' : 'visibility_off' }}
-                </span>
-              
-              </v-text-field>
+                  <span
+                    class="material-icons-outlined absolute right-3 top-5 cursor-pointer text-purple-800"
+                    @click="showPassword = !showPassword"
+                  >
+                    {{ showPassword ? "visibility" : "visibility_off" }}
+                  </span>
+                </v-text-field>
               </div>
               <div class="w-100 flex items-center justify-center mb-6">
                 <button>
@@ -156,7 +154,7 @@ const passwordRules = [
                     Reset
                   </span>
                 </div>
-                <div id="signupBtn" class="text-center">
+                <div id="signupBtn" claaster ss="text-center">
                   <span class="text-purple-800 text-center">
                     Don't have an account?
                   </span>
