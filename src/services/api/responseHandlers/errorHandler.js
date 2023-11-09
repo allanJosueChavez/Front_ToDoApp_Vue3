2e1b4a8a2245bf15aa8f92e61583113430f4c314
Allan Chávez <allancss2009@hotmail.com>
2023-11-08 20:44:56 -0600

    feat: error handling error network. No response from the backend

--- src/services/api/responseHandlers/errorHandler.js
+++ src/services/api/responseHandlers/errorHandler.js
@@ -3,8 +3,20 @@ import "vue3-toastify/dist/index.css";
 
 async function evaluateResponse(error) {
   console.log("evaluateResponse");
-  console.log(error.response);
-  if (error.response.status === 200) {
+  console.log(error);
+  if(!error.response){
+    if(error.code === 'ERR_NETWORK'){
+        toast.error("Network error.", {
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          position: "bottom-right",
+        });
+      }
+      return
+  }
+  if (error.response &&error.response.status === 200) {
     // toast.success('Success');
   } else if (error.response.status === 400 || error.response.status === 500) {
     if (error.response.data.error) {
