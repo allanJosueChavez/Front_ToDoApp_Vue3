f8a27f06d7e265e8228c8e0635a642e522f2852f
Allan Josue Chavez Sin <infodesarrollo12@minemgt.local>
2023-11-23 06:33:17 -0600

    fix: logout after a bad request. It should automatically redirect when it's 401

--- src/services/api/responseHandlers/errorHandler.js
+++ src/services/api/responseHandlers/errorHandler.js
@@ -6,19 +6,19 @@ import { useRouter } from "vue-router";
 async function evaluateResponse(error) {
   console.log("evaluateResponse");
   console.log(error);
-  if(!error.response){
-    if(error.code === 'ERR_NETWORK'){
-        toast.error("Network error. No response from the server", {
-          autoClose: 1500,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          position: "bottom-right",
-        });
-      }
-      return
+  if (!error.response) {
+    if (error.code === "ERR_NETWORK") {
+      toast.error("Network error. No response from the server", {
+        autoClose: 1500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        position: "bottom-right",
+      });
+    }
+    return;
   }
-  if (error.response &&error.response.status === 200) {
+  if (error.response && error.response.status === 200) {
     // toast.success('Success');
   } else if (error.response.status === 400 || error.response.status === 500) {
     if (error.response.data.error) {
@@ -38,24 +38,23 @@ async function evaluateResponse(error) {
         position: "bottom-right",
       });
     }
-  }else if(error.response.status === 401){
+  } else if (error.response.status === 401) {
     toast.error("Unauthorized", {
-        autoClose: 1500,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        position: "bottom-right",
-      });
-      }
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      position: "bottom-right",
+    });
     await logout();
+  }
 }
 
-
-async function logout(){
-    const router = useRouter();
-    Cookies.remove("user_jwt");
-    Cookies.remove("user_name");
-    router.push("/login");
+async function logout() {
+  const router = useRouter();
+  Cookies.remove("user_jwt");
+  Cookies.remove("user_name");
+  router.push("/login");
 }
 
 export default {
