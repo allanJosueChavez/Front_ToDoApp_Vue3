eab0fd64d51543031ac97f226c83bc291fe6efa4
Allan Josue Chavez Sin <infodesarrollo12@minemgt.local>
2023-11-29 13:57:01 -0600

    refactor: ToDoListView component

--- src/components/ToDoListView.vue
+++ src/components/ToDoListView.vue
@@ -1,15 +1,14 @@
 <script setup>
 import "../style.css";
 import "../styles/scrollbar.css";
-import { toast } from 'vue3-toastify';
-import 'vue3-toastify/dist/index.css';
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 import { ref, onMounted, computed, watch, onBeforeMount } from "vue";
 import ToDoList from "../components/ToDoList/ToDoList.vue";
 import Sidebar from "./ToDoList/Sidebar/Sidebar.vue";
 
- 
 const todos = ref([]);
- 
+
 const todos_asc = computed(() =>
   todos.value.sort((a, b) => {
     return b.createdAt - a.createdAt;
@@ -20,32 +19,17 @@ const taskLists = ref([]);
 const sortedTaskLists = computed(() => {
   return taskLists.value.slice().sort((a, b) => b.id - a.id);
 });
-
- 
 </script>
- 
-<!--  Add a search bar, default lists and counter of tasks per list. A drag an drop to order -->
+
 <template>
   <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" /> -->
   <div class="sm:flex h-screen w-full">
-  <Sidebar   
-    :listSelected="activeTodoList"
-     
-  />
-  <ToDoList 
-    
-     
-  />
- 
+    <Sidebar :listSelected="activeTodoList" />
+    <ToDoList />
   </div>
 </template>
 
-
-
 <style scoped>
-
-
-
 .container {
   width: 100%;
   height: 100%;
@@ -63,13 +47,7 @@ const sortedTaskLists = computed(() => {
   /* border-bottom: 1px solid #d4d4d4; */
   /* border-radius: 0.5rem; */
 }
-
-.animate-right {
-  position: relative;
-  animation: animateright 0.4s;
-}
-
-
+ 
 #AddTaskInput:hover {
   background-color: #d4d4d4;
 }
@@ -77,11 +55,7 @@ const sortedTaskLists = computed(() => {
 #AddTaskInput:hover::-webkit-input-placeholder {
   font-weight: 900;
   color: #6e6e6e;
-
-
 }
-
-
 
 @keyframes animateright {
   from {
