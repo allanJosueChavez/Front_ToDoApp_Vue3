65cd93bcb1061be732509ad0b4357dffc23f3881
Allan Josue Chavez Sin <infodesarrollo12@minemgt.local>
2023-12-15 09:21:37 -0600

    refactor: AccountConfirmation

--- src/views/AccountConfirmation/AccountConfirmation.vue
+++ src/views/AccountConfirmation/AccountConfirmation.vue
@@ -28,20 +28,19 @@ async function setAccountConfirmed() {
   if (token) {
     // confirmAccount
     const body = {
-      "token": token
+      token: token,
+    };
+    const response = await confirmAccount(body);
+    if (response && response.status === 200) {
+      mailConfirmed.value = true;
+      usersStore.removeUserConfirmationToken();
     }
-    const response = await confirmAccount(body)
-      if(response && response.status === 200){
-        mailConfirmed.value = true;
-        usersStore.removeUserConfirmationToken();
-      }
   }
 }
 
 const redirectResendEmailView = () => {
   router.push("/resend-email-confirmation");
 };
-
 </script>
 
 <template>
@@ -49,80 +48,91 @@ const redirectResendEmailView = () => {
     <div style="height: 10%">
       <smallLogo />
     </div>
-    <div
-      v-if="mailConfirmed"
-      id="mail-confirmed"
-      class="flex flex-col justify-center items-center space-y-2"
+    <section
       style="height: 90%"
+      class="flex flex-col justify-center items-center"
     >
-      <div>
-        <span
-          class="material-icons outlined text-lime-500"
-          :style="{ fontSize: '3em' }"
-          >check_circle</span
-        >
-      </div>
-      <div>
-        <span class="text-4xl font-bold text-center">
-          Thanks for confirming your email address!
-        </span>
-      </div>
-      <div>
-        <span class="text-xl font-semibold text-center text-gray-600">
-          Do you wish to start using ToDoFlow? Start
-          <a
-            class="cursor-pointer text-lime-500 font-bold"
-            @click="redirectToLogin"
-            >here</a
+      <div
+        v-if="mailConfirmed"
+        id="mail-confirmed"
+        class="flex flex-col justify-center items-center space-y-2 h-full"
+      >
+        <div>
+          <span
+            class="material-icons outlined text-lime-500"
+            :style="{ fontSize: '3em' }"
+            >check_circle</span
           >
-        </span>
+        </div>
+        <div>
+          <span class="text-4xl font-bold text-center">
+            Thanks for confirming your email address!
+          </span>
+        </div>
+        <div>
+          <span class="text-xl font-semibold text-center text-gray-600">
+            Do you wish to start using ToDoFlow? Start
+            <a
+              class="cursor-pointer text-lime-500 font-bold"
+              @click="redirectToLogin"
+              >here</a
+            >
+          </span>
+        </div>
       </div>
-    </div>
-    <div
-      v-if="!mailConfirmed && token"
-      class="flex flex-col justify-center items-center space-y-2"
-      style="height: 90%"
-    >
-      <div>
-        <span class="text-4xl font-bold text-center">
-          Please confirm your email address!
-        </span>
+      <div
+        v-if="!mailConfirmed && token"
+        class="flex flex-col justify-center items-center space-y-2 h-auto"
+      >
+        <div>
+          <span class="text-4xl font-bold text-center">
+            Please confirm your email address!
+          </span>
+        </div>
+        <div class="w-auto">
+          <span class="text-xl font-semibold text-center text-gray-600">
+            Hi Allan, Thanks for signing up for ToDoFlow! Please confirm your
+            email address by clicking the button
+          </span>
+        </div>
+        <div>
+          <button
+            class="bg-yellow-400 rounded-lg px-4 py-2 text-white font-semibold hover:bg-yellow-500"
+            @click="setAccountConfirmed()"
+          >
+            Confirm email address
+          </button>
+        </div>
       </div>
-      <div class="w-auto">
-        <span class="text-xl font-semibold text-center text-gray-600">
-          Hi Allan, Thanks for signing up for ToDoFlow! Please confirm your
-          email address by clicking the button
-        </span>
-      </div>
-      <div>
-        <button
-          class="bg-yellow-400 rounded-lg px-4 py-2 text-white font-semibold hover:bg-yellow-500"
-          @click="setAccountConfirmed()"
-        >
-          Confirm email address
-        </button>
-      </div>
-    </div>
 
-    <div
-      v-if="!token"
-      class="flex flex-col justify-center items-center space-y-2"
-      style="height: 90%"
-    >
-      <div class="my-4">
-        <span class="text-4xl font-bold text-center">
-          Sorry! The token is invalid or it has expired.
-        </span>
+      <div
+        v-if="!token"
+        class="flex flex-col justify-center items-center space-y-2 h-full"
+      >
+        <div class="my-4">
+          <span class="text-4xl font-bold text-center">
+            Sorry! The token is invalid or it has expired.
+          </span>
+        </div>
+
+        <div>
+          <button
+            class="bg-yellow-400 rounded-lg px-4 py-2 text-white font-semibold hover:bg-yellow-500"
+            @click="redirectResendEmailView()"
+          >
+            Resend email confirmation
+          </button>
+        </div>
       </div>
 
-      <div>
-        <button
-          class="bg-yellow-400 rounded-lg px-4 py-2 text-white font-semibold hover:bg-yellow-500"
-          @click="redirectResendEmailView()"
-        >
-          Resend email confirmation
-        </button>
+      <div class="bg-red-100 w-full flex justify-center">
+        <!-- <span class="text-xl font-semibold text-center text-gray-600  bottom-12 fixed">
+        If you need help, please contact us at
+        <a class="cursor-pointer text-lime-500 font-bold" href="mailto:achavez@mem.gob.gt"> 
+        </a>
+
+      </span> -->
       </div>
-    </div>
+    </section>
   </div>
 </template>
