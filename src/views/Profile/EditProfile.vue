b7fcc9fa4827dc5a35ba1dacf96dd9c5b934ce93
Allan Josue Chavez Sin <infodesarrollo12@minemgt.local>
2023-12-21 11:32:39 -0600

    feat: edit profile form

--- src/views/Profile/EditProfile.vue
+++ src/views/Profile/EditProfile.vue
@@ -1,6 +1,39 @@
 <template>
-   <div id="profile-edit">
-         This is the profile edit page.
+  <div class="flex">
+    <Navbar></Navbar>
+    <div
+      id="profile-edit "
+      class="px-24 py-24 h-full w-full min-h-screen bg-gradient-to-b from-purple-100 to-yellow-100"
+    >
+      <div id="title" class=" ">
+        <v-form ref="editProfileForm" fast-fail @submit.prevent="updateUser()">
+          <h1 class="mb-4 text-4xl text-left font-bold text-blue-900">
+            EDIT PROFILE
+          </h1>
+          <div class="grid gap-y-6">
+            <div class="grid grid-cols-2 gap-x-6">
+              <div  >
+                <v-text-field
+                  class="text-purple-900"
+                  label="Full name"
+                  :rules="fullNameRules"
+                  v-model="user.fullName"
+                ></v-text-field>
+              </div>
+              <div>
+                <v-text-field
+                  class="text-purple-900"
+                  label="Full name"
+                  :rules="fullNameRules"
+                  v-model="user.fullName"
+                ></v-text-field>
+              </div>
+            </div>
+          </div>
+        </v-form>
+      </div>
+      <div id="profile-edit-description" class="h-full hidden">
+            This is the profile edit page.
          It'll have:
          - a form to edit the user's profile. Vuetify components like v-text-field, v-select, v-checkbox, v-file-input, etc. will be used.
          - a button to save the changes
@@ -27,13 +60,20 @@
          - a button to go to the report a typo page
          - I gotta sync it or connect it with authentication of google, github, or fb. for that I can use the package vue-social-auth or vue-social-login
          
-
-   </div>
-
-
-
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
-    
+import Navbar from "../../components/app/navbar/verticalNavbar.vue";
+import { ref } from "vue";
 
+const fullNameRules = [
+  (v) => !!v || "Full name is required",
+  (v) => (v && v.length >= 3) || "Full name must be at least 3 characters",
+];
+
+const user = ref({
+  fullName: "",
+});
 </script>
